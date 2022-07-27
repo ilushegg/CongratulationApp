2c7ff2c2754e1bbb896ac8e25919f800526c2eff
ilushegg <marnopolskiyi@mail.ru>
2022-07-27 10:16:54 +0300

    Added the email confirmation

--- src/main/java/com/example/CongratulationApplication/service/UserService.java
+++ src/main/java/com/example/CongratulationApplication/service/UserService.java
@@ -9,16 +9,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepo userRepo;
-
-
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
 
-    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder){
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder, MailService mailService){
         this.userRepo = userRepo;
         this.passwordEncoder = passwordEncoder;
+        this.mailService = mailService;
     }
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -36,6 +38,29 @@ public class UserService implements UserDetailsService {
             return false;
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+        String link = "http://localhost:8080/activate/" + user.getActivationCode();
+        sendConfirm(user, link);
+        return true;
+    }
+
+    public void sendConfirm(User user, String link){
+        String message = String.format(
+                "Hello, %s!" +
+                        "Welcome to the application where you will never forget about your friends' birthday!" +
+                        "Please follow the following link to confirm your email address: %s",
+                user.getUsername(), link
+        );
+        mailService.send(user.getEmail(), "Welcome to the app", message);
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+        if(user == null){
+            return  false;
+        }
+        user.setActivationCode(null);
         userRepo.save(user);
         return true;
     }
